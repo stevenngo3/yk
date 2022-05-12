3cfc53c15dbfbfa9f62e72e019b70f66742b59de
stevenngo3 <s1ngo@ucsd.edu>
2022-05-11 23:47:17 -0700

    j

--- kk.java
+++ kk.java
@@ -1,6 +1,9 @@
 public class kk {
 
 // k
+    public static int sub(int n, int m){
+        return n-m;
+    }
 
 
 }
---
+++ kkTest.java
@@ -0,0 +1,10 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class kkTest {
+     @Test
+    public void addition() {
+        assertEquals(2, kk.sub(3,2));
+    }
+}
